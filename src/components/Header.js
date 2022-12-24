e195882efcdbd768cbdd8f0cb9cefe086244fe33
Gustavo <rosalesagustavo@gmail.com>
2022-12-23 23:07:54 -0500

    header styles and name of app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return <div className="Header_Sad_Fridge">
+        <div className="Title_Sad_Fridge">Sad-Fridge
+        <hr className="h"></hr>
+        </div>
+        <div>
+            <hr className="h2"></hr>
+        </div>
+    </div>;
+};
+
+export default Header;
