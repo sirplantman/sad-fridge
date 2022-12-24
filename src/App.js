5ea96762641ac25a6da07a9f40b6579e83ac7cd9
Gustavo <rosalesagustavo@gmail.com>
2022-12-23 23:33:29 -0500

    scroll when needed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Combinations from './components/Combinations';
 import Header from './components/Header';
 
 function App() {
-  const recipeNamesList = ["Recipe Name 1", "Recipe Name 2", "Recipe Name 3", "Recipe Name 4", "Recipe Name 5", "Recipe Name 6"
-                          , "Recipe Name 7", "Recipe Name 8", "Recipe Name 9", "Recipe Name 10", "Recipe Name 11"]
+  const recipeNamesList = []
   return (
     <div className="App">
       <Header />
