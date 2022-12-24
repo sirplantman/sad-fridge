1b794e525bb5dc01e41743922361c351960bd17b
Gustavo <rosalesagustavo@gmail.com>
2022-12-23 22:17:31 -0500

    testing props passing to child

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import Combinations from './components/Combinations';
 
 function App() {
+  const recipeNamesList = ["Recipe Name 1", "Recipe Name 2", "Recipe Name 3", "Recipe Name 4", "Recipe Name 5", "Recipe Name 6"
+                          , "Recipe Name 7", "Recipe Name 8", "Recipe Name 9", "Recipe Name 10", "Recipe Name 11"]
   return (
     <div className="App">
-      <Combinations />
+      <Combinations recipeNameList={recipeNamesList}/>
     </div>
   );
 }
