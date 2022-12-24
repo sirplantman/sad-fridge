47d7c02c3105c5607023e501c154ecab520430fe
Gustavo <rosalesagustavo@gmail.com>
2022-12-23 22:18:33 -0500

    mapping list of names and css styles

--- src/components/Combinations.js
+++ src/components/Combinations.js
@@ -1,5 +1,15 @@
-const Combinations = () => {
-    return <div className="Combinations"></div>;
+const Combinations = (props) => {
+    return <div className="Combinations__Wrapper">
+        <div className="Combinations">
+        {props.recipeNameList.map(names => {
+            return (
+                <div className="Recipe_Name">
+                    <p>{names}</p>
+                </div>
+            )
+        })}
+        </div>
+    </div>;
 };
 
 export default Combinations;
