a948c32e03d5877fe5f00aa27aced16293e9adf7
Gustavo <rosalesagustavo@gmail.com>
2022-12-23 23:10:47 -0500

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Combinations from './components/Combinations';
+import Header from './components/Header';
 
 function App() {
   const recipeNamesList = ["Recipe Name 1", "Recipe Name 2", "Recipe Name 3", "Recipe Name 4", "Recipe Name 5", "Recipe Name 6"
                           , "Recipe Name 7", "Recipe Name 8", "Recipe Name 9", "Recipe Name 10", "Recipe Name 11"]
   return (
     <div className="App">
+      <Header />
       <Combinations recipeNameList={recipeNamesList}/>
     </div>
   );
