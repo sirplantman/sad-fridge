34fb91f448f86f9aa8e8252cddc82ba1e5353c10
Yashaswe <amatyayasu@gmail.com>
2022-12-24 21:38:52 -0500

    add footer and update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,22 @@
 const Header = () => {
-    return <div className="Header_Sad_Fridge">
-        <div className="Title_Sad_Fridge">Sad-Fridge
-        <hr className="h"></hr>
-        </div>
-        <div>
-            <hr className="h2"></hr>
-        </div>
-    </div>;
+  return (
+    // <div className="Header_Sad_Fridge">
+    //   <div className="Title_Sad_Fridge">
+    //     Sad-Fridge
+    //     <hr className="h"></hr>
+    //   </div>
+    //   <div>
+    //     <hr className="h2"></hr>
+    //   </div>
+    // </div>
+    <div className="navbar">
+      <div>
+        <p>
+          <a href="https://sadfridge.com/">sadfridge.com</a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
